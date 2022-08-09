28741a749ac83571f18b2df19259897c48a70e93
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-08-09 21:49:39 +0900

    :sparkles: cors middleware 설정

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import cors from '@middlewares/cors';
 import userRouter from '@routers/userRouter';
 import express from 'express';
 
 const app = express();
 
-app.use('/user', userRouter);
+app.use(cors);
+app.use('/api/user', userRouter);
 
 export default app;
