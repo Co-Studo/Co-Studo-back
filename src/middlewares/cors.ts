8b6414b2dfc8fa6e62867b55e7327ca82bdc2859
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-09-23 16:31:52 +0900

    :recycle: origin not found error to not set

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -2,10 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 const cors = (req: Request, res: Response, next: NextFunction) => {
   const origins = ['http://localhost:8801'];
-  if (!req.headers.origin) {
-    throw Error('origin is not found');
-  }
-  if (origins.includes(req.headers.origin)) {
+  if (req.headers.origin && origins.includes(req.headers.origin)) {
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   }
   next();
