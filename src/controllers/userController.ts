be3051f6b37be85411754f11a369807508828f98
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-09-28 18:40:30 +0900

    :sparkles: create user by github email

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,34 +1,28 @@
 import { getParamsFormat, sendMethodResult } from '@common/utils';
 import * as userService from '@services/userService';
 import axios from 'axios';
-import type { Request, Response } from 'express';
-import type { UserRecord } from 'firebase-admin/auth';
 import { authService } from 'src/firebaseApp';
 
 const accessTokenUrl = 'https://github.com/login/oauth/access_token';
 const githubApiUrl = 'https://api.github.com';
 
-// export const getMe = sendMethodResult<UserRecord | null>(() =>
-//   userService.getMe()
-// );
+// export const getMe = sendMethodResult((req) => userService.getMe());
 
 export const getUsers = sendMethodResult(async () => {
   const users = await (await authService.listUsers()).users;
-  return users;
+  return users.map((user) => ({ email: user.email }));
 });
 
-export const createUser = sendMethodResult(async (req: Request) => {
+export const createUser = sendMethodResult(async (req) => {
   const { email, password } = req.body;
-  console.log(email, password);
   const newUserRecord = await authService.createUser({
     email,
     password,
   });
-  console.log(newUserRecord);
   return newUserRecord;
 });
 
-export const signInWithGithub = async (req: Request, res: Response) => {
+export const signInWithGithub = sendMethodResult(async (req) => {
   const { code } = req.query;
   const clientId = process.env.GITHUB_CLIENT_ID;
   const clientSecret = process.env.GITHUB_CLIENT_SECRET;
@@ -51,7 +45,16 @@ export const signInWithGithub = async (req: Request, res: Response) => {
   );
 
   const { access_token } = tokenRequest.data;
-
+  const {
+    data: { avatar_url, name },
+  } = await axios.get<{ avatar_url: string; name: string }>(
+    `${githubApiUrl}/user`,
+    {
+      headers: {
+        Authorization: `token ${access_token}`,
+      },
+    }
+  );
   const { data: emailData } = await axios.get(`${githubApiUrl}/user/emails`, {
     headers: {
       Authorization: `token ${access_token}`,
@@ -66,14 +69,14 @@ export const signInWithGithub = async (req: Request, res: Response) => {
     }
   );
 
-  // email 에 해당하는 아이다가 있으면 firebase 에서 로그인
-  // 없으면 firebase 에서 회원가입
   const user = await userService.getUserByEmail(email);
   if (user) {
-    // 로그인
-    // authService.createCustomToken(user.uid)
-    // authService.verifyIdToken()
+    return user;
   }
-  // await userService.githubSignIn(req);
-  return res.status(201).send({ ok: true });
-};
+  const newUser = await authService.createUser({
+    email,
+    photoURL: avatar_url,
+    displayName: name,
+  });
+  return newUser;
+});
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,11 +1,19 @@
 import { authService } from 'src/firebaseApp';
 
 export const getUserByEmail = async (email: string) => {
-  const user = await authService.getUserByEmail(email);
-  return user;
+  try {
+    const user = await authService.getUserByEmail(email);
+    return user;
+  } catch (e) {
+    return null;
+  }
 };
 
-export const getMe = async () => {};
+export const getMe = async (idToken: string) => {
+  // get user by firebase auth token
+  const decodedIdToken = await authService.verifyIdToken(idToken);
+  console.log(decodedIdToken);
+};
 
 // export const getMe = () => {
 //   const me = authService;
