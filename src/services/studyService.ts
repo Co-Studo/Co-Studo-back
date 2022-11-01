0553c429c7016928677f7fc4997a0be99a75488e
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-11-01 15:20:06 +0900

    :truck: remove study/tag service

--- src/services/studyService.ts
+++ src/services/studyService.ts
@@ -1,15 +1,8 @@
 import NoMatchingDocuments from '@common/exceptions/no-matching-documents';
-import { CreateTagInput } from '@dtos/create-tag.dto';
 import { CreateStudyInput, UpdateStudyInput } from '@dtos/study.dto';
 import { db } from 'src/firebaseApp';
 
 const studyRef = db.collection('study');
-const tagRef = db.collection('tag');
-
-export const createTag = async (tagInput: CreateTagInput) => {
-  const tag = await tagRef.add(tagInput);
-  return tag;
-};
 
 export const updateStudy = async (
   studyId: string,
@@ -44,22 +37,6 @@ export const getStudies = async (recruiting: boolean) => {
     return snapshot.docs.filter((doc) => doc.data().isRecruiting);
   }
   return snapshot.docs.map((doc) => doc.data());
-};
-
-export const getTags = async () => {
-  const snapshot = await tagRef.get();
-  if (snapshot.empty) {
-    throw new NoMatchingDocuments('getTags');
-  }
-  return snapshot.docs.map((doc) => doc.data());
-};
-
-export const getTagById = async (tagId: string) => {
-  const snapshot = await tagRef.doc(tagId).get();
-  if (!snapshot.exists) {
-    throw new NoMatchingDocuments('getTagById');
-  }
-  return snapshot.data();
 };
 
 export const getStudyAnnouncement = async (
