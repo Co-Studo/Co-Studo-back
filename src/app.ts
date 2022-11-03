9e57064d30975a686479c943414150ba8248a586
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-11-03 18:05:29 +0900

    :sparkles: add swagger

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,18 @@ import studyRouter from '@routers/studyRouter';
 import userRouter from '@routers/userRouter';
 import express from 'express';
 
+import swagger from './swagger';
+
 const app = express();
 
 app.use(cors);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use('/api-docs', ...swagger);
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World');
