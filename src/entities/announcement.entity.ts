4980ced4855224b07ee593b114c34ea3ef592fc7
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-11-03 18:07:16 +0900

    :sparkles: add announcement, study, tag schema

--- src/entities/announcement.entity.ts
+++ src/entities/announcement.entity.ts
@@ -1,6 +1,40 @@
 import { CoreEntity } from '@entities/core.entity';
 import { User } from '@entities/user.entity';
 
+/**
+ * @swagger
+ *  components:
+ *   schemas:
+ *    Announcement:
+ *     type: object
+ *     properties:
+ *      order:
+ *       type: string
+ *       description: 글 등록 순서
+ *      writer:
+ *       type: string
+ *      title:
+ *       type: string
+ *      content:
+ *       type: string
+ *      isFixed:
+ *       type: boolean
+ *      createdAt:
+ *       type: object
+ *       properties:
+ *        _seconds:
+ *         type: number
+ *        _nanoseconds:
+ *         type: number
+ *      updatedAt:
+ *       type: object
+ *       properties:
+ *        _seconds:
+ *         type: number
+ *        _nanoseconds:
+ *         type: number
+ */
+
 export type Announcement = {
   order: number;
   writer: Pick<User, 'name'>;
