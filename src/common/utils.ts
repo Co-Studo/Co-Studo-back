d681d2b461a9f7049e793437b7e09f021fe3607d
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-11-03 18:05:55 +0900

    :fire: update response format

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,40 +1,19 @@
-import HttpException from '@common/exceptions/http';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export const getDate = () =>
   new Date().toISOString().replace('T', ' ').substring(0, 19);
 
-export const sendMethodResult = <T>(
-  callback: (req: Request, res: Response) => T
-) => {
-  const method = async (req: Request, res: Response) => {
+export const sendMethodResult =
+  <T>(callback: (req: Request, res: Response, next: NextFunction) => T) =>
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const results: T = await callback(req, res);
-      res.send({
-        ok: true,
-        results,
-      });
+      const results = await callback(req, res, next);
+      res.send(results);
+      next();
     } catch (error) {
-      if (error instanceof HttpException) {
-        res.status(error.status).send({
-          ok: false,
-          message: error.message,
-        });
-      } else if (error instanceof Error) {
-        res.status(500).send({
-          ok: false,
-          message: error.message,
-        });
-      } else {
-        res.status(500).send({
-          ok: false,
-          message: 'Unknown Error',
-        });
-      }
+      next(error);
     }
   };
-  return method;
-};
 
 export const getParamsFormat = (config: object) => {
   const params = Object.entries(config)
