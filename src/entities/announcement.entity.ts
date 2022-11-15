1e93b476cc72ecad5da86506a1dee995df466819
Sangjin Park <58503584+healtheloper@users.noreply.github.com>
2022-11-15 20:25:15 +0900

    fix type error (#18)

--- src/entities/announcement.entity.ts
+++ src/entities/announcement.entity.ts
@@ -37,7 +37,7 @@ import { User } from '@entities/user.entity';
 
 export type Announcement = {
   order: number;
-  writer: Pick<User, 'name'>;
+  writer: string;
   title: string;
   content: string;
   isFixed: boolean;
