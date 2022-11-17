c8e43efed10aec4f25d3eeb9d10371f46ea41295
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-11-17 14:59:37 +0900

    :recycle: refactor entities - announcement, tag, user

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,20 +1,5 @@
 import { CoreEntity } from '@entities/core.entity';
-import { UserRecord } from 'firebase-admin/auth';
 
 export type User = {
   studyIds: string[];
 } & CoreEntity;
-
-/**
- * @swagger
- *  components:
- *   schemas:
- *    User:
- *     type: object
- *     properties:
- *      uid:
- *       type: string
- *      photoURL:
- *       type: string
- */
-export type AuthUser = Pick<UserRecord, 'uid' | 'photoURL'>;
